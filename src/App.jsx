8f2b6a7555c88964646341e01694ceadfffa0765
raygdev <101662915+raygdev@users.noreply.github.com>
2025-03-09 21:48:04 -0400

    Add boilerplate for shopping cart

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import './App.css'
 function App() {
   return (
     <main>
-      <DessertsSection></DessertsSection>
+      <DessertsSection />
+      <section>
+        <h2>Some Shopping Cart</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore impedit non dolor praesentium qui earum in reiciendis sunt consequatur adipisci?</p>
+      </section>
     </main>
   )
 }
