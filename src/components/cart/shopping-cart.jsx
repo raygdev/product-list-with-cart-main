0ec5f2953f954741936924222728422f62d389a7
raygdev <101662915+raygdev@users.noreply.github.com>
2025-03-10 22:53:13 -0400

    Create list for cart items and style full cart

--- src/components/cart/shopping-cart.jsx
+++ src/components/cart/shopping-cart.jsx
@@ -2,10 +2,13 @@ import { useCart } from "../../hooks/useCart";
 import './shopping-cart.css'
 
 function ShoppingCart() {
-  const { cart } = useCart()
+  const { cart, removeItem } = useCart()
   const itemCount = cart.reduce((acc, item) => {
     return acc + item.quantity
   },0)
+  const orderTotal = cart.reduce((acc, item) => {
+    return (acc + (item.quantity * item.price)) 
+  },0).toFixed(2)
   return (
     <aside aria-live="polite" className="cart">
       <div className="cart-container">
@@ -14,6 +17,40 @@ function ShoppingCart() {
             <div className="cart-container--empty">
                 <p className="text-4-semibold">Your added items will appear hear</p>
             </div>
+        )}
+
+        {cart.length > 0 && (
+            <>
+            <ul role="list">
+                {cart.map(item => (
+                    <>
+                    <li className="cart-item" key={item.id}>
+                        <div className="cart-item__wrapper">
+                          <h3 className="cart-item__title text-4-semibold">{item.name}</h3>
+                          <p className="info">
+                            <span className="cart-item__quantity text-4-semibold">{item.quantity}x</span> {"  "}
+                            <span className="cart-item__price text-4-regular">@ ${item.price}</span>{"  "}
+                            <span className="cart-item__total text-4-semibold">${(item.price * item.quantity).toFixed(2)}</span>
+                         </p>
+                        </div>
+                        <button
+                          className="cart-item__button"
+                          aria-label={`remove all ${item.name}`}
+                          onClick={() => removeItem(item.id)}
+                        >
+                            <img src="/assets/images/icon-remove-item.svg" alt="" />
+                        </button>
+                        
+                    </li>
+                    <hr></hr>
+                    </>
+                ))}
+            </ul>
+            <div>
+                <p>Order Total</p>
+                <span>${orderTotal}</span>
+            </div>
+            </>
         )}
       </div>
     </aside>
